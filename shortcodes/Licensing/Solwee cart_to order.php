497f6a1a4aad0a695673f8e0e8707f9a1364aa62
fastmediahouse <marco@fastmediahouse.com>
2025-07-09 20:12:35 +0200

    Update Solwee cart_to order.php

--- shortcodes/Licensing/Solwee cart_to order.php
+++ shortcodes/Licensing/Solwee cart_to order.php
@@ -1,7 +1,1 @@
-// WPCode Snippet Name: solwee_cart_to_order_meta
 
-add_action('woocommerce_checkout_create_order_line_item', function($item, $cart_item_key, $values, $order) {
-    if (isset($values['solwee_license'])) {
-        $item->add_meta_data('solwee_license', $values['solwee_license'], true);
-    }
-}, 10, 4);
